ccc0157cfa0ee0f20d4c9bcbe1fd6bf7455939ca
heyjochen <jochen@stierberger.com>
2022-07-17 19:26:18 -0500

    remove comments

--- js/main.js
+++ js/main.js
@@ -1,30 +1,25 @@
-//Grab artist name on button
-document.querySelector(".poster-header__nav").addEventListener("click", (e) => {
+document.querySelector('.poster-header__nav').addEventListener('click', (e) => {
   e.preventDefault();
   const { target } = e;
   const { id } = target;
-  const findDash = id.indexOf("-");
+  const findDash = id.indexOf('-');
   const IDsliced = id.slice(findDash + 1, findDash.length);
   showArtistPoster(IDsliced);
 });
 
-//Grab an artists artworks
 async function showArtistPoster(artistID) {
   try {
-    //read artist JSON
     let artist = await fetch(
       `https://api.artic.edu/api/v1/artists/${artistID}`
     );
     let artistObject = await artist.json();
 
-    //generate random Artwork ID
     const randomIndex = Math.floor(
       Math.random() * (artistObject.data.artwork_ids.length - 1) + 1
     );
     const randomObjectID = artistObject.data.artwork_ids[randomIndex];
     console.log(randomObjectID);
 
-    //read Artwork JSON itself
     let artwork = await fetch(
       `https://api.artic.edu/api/v1/artworks/${randomObjectID}`
     );
@@ -32,7 +27,6 @@ async function showArtistPoster(artistID) {
 
     const poster = new Poster(artworkObject);
     await poster.servePoster();
-    
   } catch (err) {
     console.error(`Could not get poster: ${err}`);
   }
@@ -46,13 +40,13 @@ class Poster {
     this.dimensions = artworkInfo.data.dimensions;
     this.artworkID = artworkInfo.data.id;
     this.title = artworkInfo.data.title;
-    this.iiifURL = artworkInfo.config["iiif_url"];
-    this.imageID = artworkInfo.data["image_id"];
+    this.iiifURL = artworkInfo.config['iiif_url'];
+    this.imageID = artworkInfo.data['image_id'];
     this.thumbnail = artworkInfo.data.thumbnail;
     this.color = artworkInfo.data.color;
   }
 
-  async servePoster(){
+  async servePoster() {
     await this.displayImage();
     await this.addTitle();
     await this.addArtist();
@@ -64,51 +58,51 @@ class Poster {
 
   async displayImage() {
     return new Promise((resolve) => {
-      const image = document.querySelector("#poster-main__img");
-        // Fallback for images that don't exist
-        if (this.thumbnail === null || this.thumbnail === undefined) {
-          image.src =
-            "https://raw.githubusercontent.com/heyjochen/Posters-Chicago/main/assets/210422_js_STI6262.jpg";
-          } else {
-          const imagePath = "/full/843,/0/default.jpg";
-          const imageURL = `${this.iiifURL}/${this.imageID}${imagePath}`;
-          image.src = imageURL;
-        }
-      image.addEventListener("load", () => { 
+      const image = document.querySelector('#poster-main__img');
+
+      if (this.thumbnail === null || this.thumbnail === undefined) {
+        image.src =
+          'https://raw.githubusercontent.com/heyjochen/Posters-Chicago/main/assets/210422_js_STI6262.jpg';
+      } else {
+        const imagePath = '/full/843,/0/default.jpg';
+        const imageURL = `${this.iiifURL}/${this.imageID}${imagePath}`;
+        image.src = imageURL;
+      }
+      image.addEventListener('load', () => {
         resolve(image);
       });
     });
   }
 
   async addTitle() {
-    const title = document.querySelector(".poster-main__h1");
+    const title = document.querySelector('.poster-main__h1');
     title.textContent = this.title;
   }
 
   async addArtist() {
-      const artistName = document.querySelector("#poster-main__artistName")
-      artistName.innerHTML = this.artistName;
+    const artistName = document.querySelector('#poster-main__artistName');
+    artistName.innerHTML = this.artistName;
   }
 
   async addAlt() {
     if (this.description !== null) {
-      document.querySelector(".poster-main__h2").textContent = this.description;
+      document.querySelector('.poster-main__h2').textContent = this.description;
     }
   }
 
   async addDimensions() {
-    document.querySelector(".poster-main__h3").textContent = this.dimensions;
+    document.querySelector('.poster-main__h3').textContent = this.dimensions;
   }
 
   async addDepartment() {
-    document.getElementById("poster-main__department").textContent =
+    document.getElementById('poster-main__department').textContent =
       this.department;
   }
 
   async changeTextShadow() {
     if (this.color !== null) {
       document.querySelector(
-        ".poster-main__imgInfo"
+        '.poster-main__imgInfo'
       ).style.textShadow = `1px 1px 0px hsl(${this.color.h}, ${this.color.s}%, ${this.color.l}%)`;
     }
   }
